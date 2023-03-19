831e5058dd3b0870530847372d68a332705a7364
Fernando Estrada <Estradafr@yahoo.com>
2023-03-19 01:04:21 -0700

    completed with_router activity

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './styles/App.css';
 
-import {useEffect, useState} from 'react';
+import {Fragment, useEffect, useState} from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {Gallery} from './components/Gallery';
 import {SearchBar} from './components/SearchBar';
+import {ArtistView} from './components/artistView';
+import {AlbumView} from './components/albumView';
 
 export function App() {
 	let [search, setSearch] = useState('');
@@ -22,9 +25,30 @@ export function App() {
 
 	return (
 		<div className="App">
-			<SearchBar setSearch={setSearch} />
-			{message}
-			<Gallery data={data} />
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<>
+								<SearchBar setSearch={setSearch} />
+								{message}
+								<Gallery data={data} />
+							</>
+						}
+					/>
+
+					<Route
+						path="/album/:id"
+						element={<AlbumView />}
+					/>
+
+					<Route
+						path="/artist/:id"
+						element={<ArtistView />}
+					/>
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
